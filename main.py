f325f116c1654997862d1303522e7e3ae2636c7c
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-07-28 14:18:51 +0200

    Added blacklist, etc.

--- main.py
+++ main.py
@@ -5,60 +5,13 @@ import pandas as pd
 from tqdm import tqdm
 import concurrent.futures
 from get_data import get_spotify_chart
+from model import *
 
 
 def batch(iterable, n=1):
     l = len(iterable)
     for ndx in range(0, l, n):
         yield iterable[ndx : min(ndx + n, l)]
-
-
-db = SqliteDatabase("spotify.db")
-
-
-class BaseModel(Model):
-    class Meta:
-        database = db
-
-
-class Chart(BaseModel):
-    id = AutoField()
-    name = CharField()
-    url = CharField()
-    interval = CharField()
-    genre = CharField(null=True)
-
-
-class Region(BaseModel):
-    id = AutoField()
-    country_code = CharField(unique=True)
-    name = CharField()
-
-
-class Artist(BaseModel):
-    id = AutoField()
-    name = CharField()
-
-
-class Track(BaseModel):
-    id = AutoField()
-    name = CharField()
-    spotify_id = CharField(index=True, unique=True)
-    # artist_id = ForeignKeyField(Artist, backref="artists")
-
-
-class ChartEntry(BaseModel):
-    id = AutoField()
-    date = DateField()
-    position = IntegerField()
-    streams = IntegerField()
-    chart_id = ForeignKeyField(Chart)
-    region_id = ForeignKeyField(Region)
-    track_id = ForeignKeyField(Track)
-
-
-db.connect()
-db.create_tables([Chart, Region, Track, Artist, ChartEntry])
 
 
 def populate_tables():
@@ -97,15 +50,51 @@ def add_track(name, spotify_id):
         track_id = Track.create(name=name, spotify_id=spotify_id)
     return track_id
 
-def add_tracks(tracks):
+
+def add_blacklist(url):
+    try:
+        bid = Blacklist.get(url=url)
+    except:
+        bid = Blacklist.create(url=url)
+    return bid
+
+
+def add_artists(data):
+    data = {t.artist: t for t in data}
+    existing_ids = set([r.name for r in Artist.select(Artist.name)])
+
+    add_ids = set(data.keys()) - existing_ids
+    data = [{"name": t.artist} for k, t in data.items() if t.artist in add_ids]
+    print("Adding {} artists".format(len(data)))
     with db.atomic():
-        track_ids = Track.insert_many(tracks).on_conflict("ignore").execute()
-    return track_ids
+        ids = Artist.insert_many(data).on_conflict("ignore").execute()
+    return ids
+
+
+def add_tracks(data):
+    tracks = {t.spotify_id: t for t in data}
+    existing_ids = set([r.spotify_id for r in Track.select(Track.spotify_id)])
+
+    add_ids = set(tracks.keys()) - existing_ids
+    tracks = [
+        {
+            "name": t.name,
+            "artist_id": Artist.get(name=t.artist),
+            "spotify_id": t.spotify_id,
+        }
+        for k, t in tracks.items()
+        if t.spotify_id in add_ids
+    ]
+    print("Adding {} tracks".format(len(tracks)))
+    with db.atomic():
+        Track.insert_many(tracks).on_conflict("ignore").execute()
+
 
 def add_chart_entry(date, position, streams, chart_id, region_id, track_id):
     # try:
     #     chart_entry_id = ChartEntry.get(date=date, chart_id=chart_id, region_id=region_id, track_id=track_id)
     # except:
+
     chart_entry_id = ChartEntry.create(
         date=date,
         position=position,
@@ -116,6 +105,28 @@ def add_chart_entry(date, position, streams, chart_id, region_id, track_id):
     )
     return chart_entry_id
 
+
+def add_chart_entries(data):
+    existing_tracks = {
+        r.spotify_id: r.id for r in Track.select(Track.id, Track.spotify_id)
+    }
+    data = [
+        {
+            "date": d.date,
+            "position": d.position,
+            "streams": d.streams,
+            "chart_id": chart_ids["{}_{}".format(d.chart, d.interval)],
+            "region_id": region_ids[d.region],
+            "track_id": existing_tracks[d.spotify_id],
+        }
+        for d in data
+        if d.spotify_id is not None
+    ]
+    print("Adding {} chart entries".format(len(data)))
+    with db.atomic():
+        ChartEntry.insert_many(data).on_conflict("ignore").execute()
+
+
 if __name__ == "__main__":
     chart_ids, region_ids = populate_tables()
 
@@ -123,18 +134,38 @@ if __name__ == "__main__":
     from datetime import date
 
     start_date = date(2017, 1, 1)
-    dates = get_dates(start_date)
+    daily_dates = get_dates(start_date)
+    charts = list(
+        ChartEntry.select(ChartEntry.date, ChartEntry.chart_id, ChartEntry.region_id)
+        .distinct()
+        .dicts()
+    )
+    composite_keys = set()
+    for c in tqdm(charts):
+        composite_keys.add("{}_{}_{}".format(c["chart_id"], c["region_id"], c["date"]))
+
+    blacklist_urls = list(Blacklist.select(Blacklist.url))
+    print(blacklist_urls)
 
     urls = []
     charts = Chart.select()
     regions = Region.select()
     for c in charts:
         for r in regions:
-            for d in dates:
-                url = "https://spotifycharts.com/{}/{}/{}/{}/download".format(
-                    c.url, r.country_code, c.interval, d
-                )
-                urls.append((url, c.url, r.country_code, c.interval, d))
+            for d in daily_dates:
+                if c.interval == "daily":
+                    composite_key = "{}_{}_{}".format(
+                        chart_ids["{}_{}".format(c.url, c.interval)],
+                        region_ids[r.country_code],
+                        d,
+                    )
+                    if composite_key not in composite_keys:
+                        url = "https://spotifycharts.com/{}/{}/{}/{}/download".format(
+                            c.url, r.country_code, c.interval, d
+                        )
+
+                        if url not in blacklist_urls:
+                            urls.append((url, c.url, r.country_code, c.interval, d))
 
     reqs = 0
     datas = []
@@ -150,71 +181,18 @@ if __name__ == "__main__":
                 data = future.result()
                 if data:
                     datas.extend(data)
+                else:
+                    print("Added {} to blacklist".format(url))
+                    add_blacklist(url[0])
 
-                if len(datas) > 200 * 200:
+                if len(datas) > (200 * 200):
                     id_name = {}
                     new_ids = []
-                    for d in datas:
-                        id_name[d.spotify_id] = d.name
-                        new_ids.append(d.spotify_id)
-
-                    existing_ids = set([r.spotify_id for r in Track.select(Track.spotify_id)])
-                    add_ids = set(new_ids) - existing_ids
-                    tracks = [{"name": id_name[spotify_id], "spotify_id": spotify_id} for spotify_id in add_ids]
-                    track_ids = add_tracks(tracks)
-                    print("Added {} tracks".format(len(tracks)))
+                    add_artists(datas)
+                    add_tracks(datas)
+                    add_chart_entries(datas)
                     datas = []
-                    # chart_id = chart_ids["{}_{}".format(d.chart, d.interval)]
-                    # region_id = region_ids[d.region]
-                    # chart_entry_id = add_chart_entry(
-                    #     d.date, d.position, d.streams, chart_id, region_id, track_id
-                    # )
             except Exception as e:
                 print("Exception", e)
             reqs += 1
             print(reqs, "/", len(urls))
-
-    exit(0)
-
-    print("Reading historical data from csv file")
-    DATA = "test.csv"  # "./datasets/spotify/historical_chart.csv"
-    spotify_df = pd.read_csv(DATA, index_col=0)
-    spotify_df["date"] = pd.to_datetime(spotify_df["date"]).dt.date
-    spotify_df = spotify_df.dropna()
-
-    print("Adding unique tracks")
-    tracks_df = spotify_df.drop_duplicates(subset="spotify_id", keep="first")
-    for idx, row in tqdm(tracks_df.iterrows(), total=tracks_df.shape[0]):
-        try:
-            track = Track.create(spotify_id=row["spotify_id"], name=row["track_name"],)
-        except:
-            pass
-
-    print("Mapping database IDs to vendor identifier")
-    track_ids = Track.select().where(
-        Track.spotify_id.in_(tracks_df["spotify_id"].to_list())
-    )
-    id_spotify = {}
-    for q in track_ids:
-        id_spotify[q.spotify_id] = q.id
-
-    # create chart mapping ID
-    spotify_df["chart_mapping_id"] = (
-        spotify_df["chart"].map(str) + "_" + spotify_df["chart_type"]
-    )
-    spotify_df["region_id"] = spotify_df["region"].map(region_ids)
-    spotify_df["chart_id"] = spotify_df["chart_mapping_id"].map(chart_ids)
-    spotify_df["track_id"] = spotify_df["spotify_id"].map(id_spotify)
-    spotify_df = spotify_df[
-        ["date", "position", "streams", "chart_id", "region_id", "track_id"]
-    ]
-
-    print("Building list queries")
-    spotify_df = spotify_df.to_dict("records")
-
-    print("Adding Chart Entries")
-    N = 50000
-    for b in batch(spotify_df, N):
-        print("Adding {} rows".format(N))
-        with db.atomic():
-            ChartEntry.insert_many(b).execute()
---
+++ model.py
@@ -0,0 +1,53 @@
+from peewee import *
+
+
+db = SqliteDatabase("spotify.db")
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Chart(BaseModel):
+    id = AutoField()
+    name = CharField()
+    url = CharField()
+    interval = CharField()
+    genre = CharField(null=True)
+
+
+class Region(BaseModel):
+    id = AutoField()
+    country_code = CharField(unique=True)
+    name = CharField()
+
+
+class Artist(BaseModel):
+    id = AutoField()
+    name = CharField()
+
+
+class Track(BaseModel):
+    id = AutoField()
+    name = CharField()
+    spotify_id = CharField(index=True, unique=True)
+    artist_id = ForeignKeyField(Artist)
+
+
+class ChartEntry(BaseModel):
+    id = AutoField()
+    date = DateField()
+    position = IntegerField()
+    streams = IntegerField()
+    chart_id = ForeignKeyField(Chart)
+    region_id = ForeignKeyField(Region)
+    track_id = ForeignKeyField(Track)
+
+class Blacklist(BaseModel):
+    id = AutoField()
+    url = CharField()
+
+db.connect()
+db.create_tables([Chart, Region, Track, Artist, ChartEntry, Blacklist])
+
